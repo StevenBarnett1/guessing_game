7fab7cc3bbefaad36b3a943e54dfe56b133bb950
Robert Kauth <kauthr@gmail.com>
2021-06-17 13:57:05 -0700

    added random num

--- guessing-game.js
+++ guessing-game.js
@@ -1,34 +1,33 @@
-const readline = require("readline")
+const readline = require('readline');
 
 const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdin
-})
+	input: process.stdin,
+	output: process.stdin,
+});
 
-let secretNumber = 10
+let randomInRange = (min, max) => {
+	return Math.round(Math.random() * (max - min) + min);
+};
 
+let secretNumber = randomInRange(1, 50);
 
-let checkGuess = num => {
-    if (num > secretNumber) {
-        console.log("Too High")
-        return false
-    } else if (num < secretNumber) {
-        console.log("Too Low")
-        return false
-    } else {
-        console.log("Correct")
-        return true
-    }
-
-}
+let checkGuess = (num) => {
+	if (num > secretNumber) {
+		console.log('Too High');
+		return false;
+	} else if (num < secretNumber) {
+		console.log('Too Low');
+		return false;
+	} else {
+		console.log('Correct');
+		return true;
+	}
+};
 
 let askGuess = () => {
-    rl.question("Enter A Guess: ", (answer) => {
-        checkGuess(Number(answer))
-        rl.close()
-    })
-
-}
-
-
-askGuess()
+	rl.question('Enter A Guess: ', (answer) => {
+		if (!checkGuess(Number(answer))) askGuess();
+		else rl.close();
+	});
+};
+askGuess();
