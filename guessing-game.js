cb1b621996e9e9ec18440a3ad5b7193ac2baff83
Robert Kauth <kauthr@gmail.com>
2021-06-17 15:05:27 -0700

    added bonus

--- guessing-game.js
+++ guessing-game.js
@@ -9,14 +9,16 @@ let randomInRange = (min, max) => {
 	return Math.round(Math.random() * (max - min) + min);
 };
 
-let askRange = () => {
-    rl.question("Enter a min and max separated with a space: ", answer => {
-        askGuess(randomInRange(...answer.split(" ").map(x => Number(x))))
-    })
+let askRange = (numOfTries) => {
+	rl.question('Enter a min and max separated with a space: ', (answer) => {
+		askGuess(
+			numOfTries,
+			randomInRange(...answer.split(' ').map((x) => Number(x)))
+		);
+	});
+};
 
-}
-
-let checkGuess = (num,secretNumber) => {
+let checkGuess = (num, secretNumber) => {
 	if (num > secretNumber) {
 		console.log('Too High');
 		return false;
@@ -28,12 +30,23 @@ let checkGuess = (num,secretNumber) => {
 		return true;
 	}
 };
+let askLimit = () => {
+	rl.question('How many tries do you want? ', (answer) => {
+		askRange(Number(answer));
+	});
+};
 
-let askGuess = (secretNumber) => {
+let askGuess = (numAttempts, secretNumber) => {
+	console.log('NumAttempts' + numAttempts);
+
 	rl.question('Enter A Guess: ', (answer) => {
-		if (!checkGuess(Number(answer),secretNumber)) askGuess(secretNumber);
+		numAttempts--;
+		if (!numAttempts) {
+			console.log('YOU LOSE');
+			rl.close();
+		} else if (!checkGuess(Number(answer), secretNumber)) askGuess(numAttempts, secretNumber);
 		else rl.close();
 	});
 };
 
-askRange()
+askLimit();
